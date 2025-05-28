63e0563541902e023bb46151611dc7b95f6edade
AhmadSamim <56501116+AhmadSamim@users.noreply.github.com>
2025-05-28 08:09:39 -0700

    Made changes to jin caching metada

--- app/src/main/java/com/sheesha/ruqyacenter/data/quranapi/jinCatchingMeta.kt
+++ app/src/main/java/com/sheesha/ruqyacenter/data/quranapi/jinCatchingMeta.kt
@@ -3,207 +3,208 @@ package com.sheesha.ruqyacenter.data.quranapi
  val JinCatchingMeta = mapOf(
     "2:148" to Pair(
         "Summon the jinn",
-        "To call the jinn to the catcher's body, recite the following verses until you notice signs that the jinn catcher is possessed..."
+        "To call the jinn to the catcher’s body, recite the following verses until you notice signs that the jinn catcher is possessed. When the jinn catcher catches the jinn, they will begin to show abnormal signs like twitching, uncontrollable hand shaking, head moving around, body shivering, making weird sounds"
     ),
     "27:30-31" to Pair(
         "Summon the jinn",
-        "It is from Sulaiman, and it is (in the following words): With the name of God, the All-Merciful, the Very-Merciful. (30) Do not rebel against me, and come to me in submission."
+        ""
     ),
     "37:158" to Pair(
         "Summon the jinn",
-        "... while the Jinns already know that they are bound to be seized."
+        ""
     ),
     "36:32" to Pair(
         "Summon the jinn",
-        "All of them are but to be assembled together (and) to be arraigned before Us."
+        ""
     ),
     "55:1-5" to Pair(
         "If the jinn is mute",
-        "The Compassionate. Has taught the Quran. He created man. And taught him clear expression."
+        "Clever magicians send mute jinn to possess people. They do this because if the jinn can't talk, it would be impossible to get any information from the jinn and it would be difficult to get the jinn to leave. In this case, read the following verses to help it talk. If the jinn still can't talk, you can keep reciting Ayat Al-Kursi until it burns to death or leaves on its own."
     ),
     "41:21" to Pair(
         "If the jinn is mute",
-        "... They Said: We were made to speak by God, the One who has made every thing to speak..."
+        ""
     ),
     "113:4" to Pair(
         "If the jinn is mute",
-        "... And from the evil of the blowers in knots..."
+        ""
     ),
     "37:92" to Pair(
         "If the jinn is mute",
-        "What's wrong with you that you can't speak."
+        ""
     ),
     "20:27-28" to Pair(
         "If the jinn is mute",
-        "And loose a knot from my tongue, (27) That they may understand my saying."
+        ""
     ),
     "51:23" to Pair(
         "If the jinn is mute",
-        "And by the Lord of the heavens and the earth, it is the truth, even as (it is true) that ye speak."
+        ""
     ),
     "25:45" to Pair(
         "If the jinn is violent or threatens",
-        "Have you not seen how your Lord spread the shadow. If He willed, He could have made it - still"
+        ""
     ),
     "69:30-32" to Pair(
         "If the jinn is violent or threatens",
-        "[Allah will say], \"Seize him and shackle him. Then into Hellfire drive him. Then into a chain whose length is seventy cubits insert him.\""
+        ""
     ),
     "6:13" to Pair(
         "If the jinn threatens to inform the sorcerer",
-        "And to Him belongs whatsoever exists in the night and the day, and He is the All-Hearing, the All-Knowing."
+        ""
     ),
     "39:62-63" to Pair(
         "If the jinn doesn't know God",
-        "Allâh is the Creator of all things, and He is the Wakîl (Trustee, Disposer of affairs, Guardian) over all things. To Him belong the keys of the heavens and the earth. And those who disbelieve in the Ayât of Allâh, such are they who will be the losers."
+        ""
     ),
     "24:35" to Pair(
         "If the jinn doesn't know God",
-        "God is the Light of the heavens and the earth."
+        ""
     ),
     "2:257" to Pair(
         "If the jinn doesn't know God",
-        "God is the Protector of those who believe. He brings them out of the depths of darkness into the light."
+        ""
     ),
     "3:19" to Pair(
         "If the jinn doesn't know Islam",
-        "Truly, the (recognized) religion in the sight of God is Islam."
+        ""
     ),
     "51:56" to Pair(
         "If the jinn doesn't know Islam",
-        "I did not create the Jinns and the human beings except for the purpose that they should worship Me."
+        ""
     ),
     "6:162-163" to Pair(
         "If the jinn doesn't know Islam",
-        "Say, \"My prayer, my offering, my life and my death are for God, the Lord of all the worlds. For Him there is no partner. And thus I have been commanded, and I am the first one to submit.\""
+       ""
     ),
     "9:1" to Pair(
         "If the jinn has pact with magician",
-        "Here is a disavowal (proclaimed) by God and His Messenger against the Mushriks (polytheists) with whom you have a treaty."
+       ""
     ),
     "4:145" to Pair(
         "If the jinn lied about its color",
-        "Surely, the hypocrites are in the lowest level of the Fire, and you shall never find for them a helper"
+        ""
     ),
     "50:22" to Pair(
         "Find the black magic",
-        "Now We have removed your veil from you; so your sight today is sharp."
+        ""
     ),
     "10:81" to Pair(
         "Destroy the black magic",
-        "So, when they had cast, Musa said, \"All that you have brought is magic. God will certainly bring it to naught. Be assured that God does not set right the work of the mischief-makers.\""
+       ""
     ),
     "59:21" to Pair(
         "Destroy the black magic",
-        "If We had sent down this Qur'an upon a mountain, you would have seen it humbled and coming apart from fear of God. And these examples We present to the people that perhaps they will give thought."
+       ""
     ),
     "21:30" to Pair(
         "Destroy the black magic",
-        "Did the disbelievers not observe that the heavens and the earth were closed, then We opened them?"
+        ""
     ),
     "25:23" to Pair(
         "Destroy the black magic",
-        "And We will proceed to whatever deeds they did, and will turn them into scattered dust."
+       ""
     ),
     "8:11" to Pair(
         "Destroy the black magic",
-        "And we sent down upon you water from the heavens, so that He might purify you with it, and remove from you the impurity of Satan, and so that He might strengthen your hearts and make (your) feet firm therewith."
+       ""
     ),
     "16:26" to Pair(
         "Destroy the black magic",
-        "Then (the command of) God came upon their buildings (uprooting them) from the foundations. So roofs fell down upon them from above, and the chastisement came to them from where they could not even imagine."
+        ""
     ),
     "21:69" to Pair(
         "Conditional verses",
-        "We said: O fire, be coolness and peace for Abraham"
+        ""
     ),
     "30:47" to Pair(
         "If the jinn is afraid to leave",
-        "And it was due on Us to help the believers."
+        ""
     ),
     "15:21" to Pair(
         "If the jinn asks where to go",
-        "We have treasures of everything, but We send it down only in a determined quantity."
+        ""
     ),
     "99:1-2" to Pair(
         "If the jinn doesn't want to leave",
-        "When the earth will be trembled with its quake, And the earth will bring forth its burdens"
+       ""
     ),
     "35:36-38" to Pair(
         "If the jinn doesn't want to leave",
-        "And for those who disbelieve will be the fire of Hell. [Death] is not decreed for them so they may die, nor will its torment be lightened for them. Thus do we recompense every ungrateful one..."
+       ""
     ),
     "27:30-31" to Pair(
         "If the jinn refuses to leave",
-        "It is from Sulaiman, and it is (in the following words): With the name of God, the All-Merciful, the Very-Merciful. Do not rebel against me, and come to me in submission."
+       ""
     ),
     "36:82" to Pair(
         "If the jinn refuses to leave",
-        "His practice, when He intends to do something, is no more than He says, \"Be\", and it comes to be."
+        ""
+
     ),
     "4:76" to Pair(
         "If the jinn refuses to leave",
-        "The believers fight in the way of God, and the disbelievers fight in the way of Taghut. So, fight the friends of Satan. No doubt, the guile of Satan is feeble."
+        ""
     ),
     "17:45" to Pair(
         "If the jinn is afraid of other jinn",
-        "And when thou recitest the Qur'an we place between thee and those who believe not in the Hereafter a hidden barrier"
+        ""
     ),
     "33:3" to Pair(
         "If the jinn is afraid of sorcerer",
-        "And put thy trust in God, for God is sufficient as Trustee."
+        ""
     ),
     "3:61" to Pair(
         "If the jinn is lying",
-        "... the Curse of Allâh upon those who lie."
+        ""
     ),
     "17:81" to Pair(
         "If the jinn says you are lying",
-        "And say: Truth hath come and falsehood hath vanished away. Lo! falsehood is ever bound to vanish."
+        ""
     ),
     "76:4" to Pair(
         "If the jinn tries to run away",
-        "We have prepared, for the disbelievers, shackles and iron-collars and a blazing Fire."
+        ""
     ),
     "37:24-26" to Pair(
         "If the jinn tries to run away",
-        "And stop them, for they must be questioned. What aileth you that ye help not one another? Nay, but this Day they make full submission."
+        ""
     ),
     "86:8-17" to Pair(
         "If the jinn tries to run away",
-        "Surely He is Powerful to bring him back On a day when all the secrets will be searched out..."
+        ""
     ),
     "7:196" to Pair(
         "If the jinn asks who you are",
-        "Lo! my Protecting Friend is God Who revealeth the Scripture. He befriendeth the righteous."
+        ""
     ),
     "30:17-19" to Pair(
         "If the jinn's family has been killed",
-        "So, proclaim God's purity (from shirk) when you see the evening and when you see the morning..."
+        ""
     ),
     "30:30" to Pair(
         "If the jinn doesn't like its shape",
-        "So, set your face to the Faith uprightly, this (faith) being the nature designed by God on which He has created the mankind..."
+        ""
     ),
     "3:26" to Pair(
         "If the jinn says it is a king",
-        "Say: O God! Owner of Sovereignty! Thou givest sovereignty unto whom Thou wilt, and Thou withdrawest sovereignty from whom Thou wilt..."
+        ""
     ),
     "38:20" to Pair(
         "If the jinn says it is a king",
-        "We made his kingdom strong and gave him wisdom and decisive speech."
+        ""
     ),
     "30:21" to Pair(
         "If the jinn is a lover jinn",
-        "And it is among His signs that He has created for you wives from among yourselves, so that you may find tranquility in them..."
+        ""
     ),
     "35:6" to Pair(
         "If the jinn works with Satan",
-        "Surely Shaitan (Satan) is an enemy for you. So, take him as an enemy. He only invites his group (to falsehood) so that they become inmates of the blazing fire."
+        ""
     ),
     "14:22" to Pair(
         "If the jinn works with Satan",
-        "The Satan will say when the matter will have been decided, \"God promised you a truthful promise while I gave you a promise and did not fulfill it..."
+        ""
     ),
     "1:1-7" to Pair(
         "If the jinn is forgetful",
@@ -211,22 +212,22 @@ package com.sheesha.ruqyacenter.data.quranapi
     ),
     "48:4" to Pair(
         "If the jinn is forgetful",
-        "He (God) is such that He sent down tranquility into the hearts of the believers, so that they grow more in faith in addition to their (existing) faith."
+        ""
     ),
     "95:4" to Pair(
         "If the jinn is forgetful",
-        "We have created man in the best composition"
+        ""
     ),
     "36:58" to Pair(
         "To calm the jinn down",
-        "Salam \"(Peace upon you) is the word (they receive) from Merciful Lord."
+        ""
     ),
     "36:64" to Pair(
         "To show the jinn Hell",
-        "(Now) this is the Jahannam of which you were consistently warned. Enter it today, because you have been persistently denying (the truth)."
+        ""
     ),
     "78:31-36" to Pair(
         "To show the jinn Paradise",
-        "Of course, the God-fearing will have great achievement, Gardens and grapes, And buxom maidens of matching age..."
+        ""
     )
 )
